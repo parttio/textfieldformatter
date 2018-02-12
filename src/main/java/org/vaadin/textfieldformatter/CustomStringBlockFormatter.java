bb9e507bfd65bf06afb46b7618fcd810ce035fce
Johannes Häyry <johannes.hayry@vaadin.com>
2018-02-13 00:22:01 +0200

    Add some javadoc

--- src/main/java/org/vaadin/textfieldformatter/CustomStringBlockFormatter.java
+++ src/main/java/org/vaadin/textfieldformatter/CustomStringBlockFormatter.java
@@ -4,13 +4,33 @@ import com.vaadin.ui.AbstractTextField;
 
 public class CustomStringBlockFormatter extends AbstractTextFieldFormatterExtension {
 
-	public CustomStringBlockFormatter(AbstractTextField field, int[] blocks, String[] delimiters, ForceCase forceCase, boolean numericOnly) {
+	/**
+	 * Creates an instance of CustomStringBlockFormatter
+	 * 
+	 * @param field
+	 *            the AbstractTextField to create the extension for
+	 * @param blocks
+	 *            an array configuring the character blocks. The length of the array
+	 *            is the number of character blocks. The integers in the array are
+	 *            the length of the blocks.
+	 * @param delimiters
+	 *            the delimiter characters to be used between the character blocks
+	 * @param forceCase
+	 *            enumeration to control letter case of characters. One of
+	 *            ForceCase.NONE don't touch letter case. ForceCase.LOWER transform
+	 *            characters to lower case. ForceCase.UPPER transform characters to
+	 *            upper case.
+	 * @param numericOnly
+	 *            true for allowing numeric characters only. Defaults to false.
+	 */
+	public CustomStringBlockFormatter(AbstractTextField field, int[] blocks, String[] delimiters, ForceCase forceCase,
+			boolean numericOnly) {
 		super(field);
 		getState().formatBlocks = blocks;
 		getState().delimiters = delimiters;
 		if (forceCase == ForceCase.UPPER) {
 			getState().lowercase = false;
-			getState().uppercase = true;			
+			getState().uppercase = true;
 		} else if (forceCase == ForceCase.LOWER) {
 			getState().lowercase = true;
 			getState().uppercase = false;
@@ -21,18 +41,16 @@ public class CustomStringBlockFormatter extends AbstractTextFieldFormatterExtens
 	public CustomStringBlockFormatter(AbstractTextField field, int[] blocks, String[] delimiters, ForceCase forceCase) {
 		this(field, blocks, delimiters, forceCase, false);
 	}
-	
+
 	public CustomStringBlockFormatter(AbstractTextField field, int[] blocks, ForceCase forceCase) {
 		this(field, blocks, null, forceCase);
 	}
-	
+
 	public CustomStringBlockFormatter(AbstractTextField field, int[] blocks) {
 		this(field, blocks, null, ForceCase.NONE);
 	}
-	
+
 	public enum ForceCase {
-		NONE,
-		UPPER,
-		LOWER
+		NONE, UPPER, LOWER
 	}
 }
